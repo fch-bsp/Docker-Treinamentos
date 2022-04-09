a30dea11bd8b4068eb42ab284fd5b2b549e81ba8
nandohoras <102867453+fch-bsp@users.noreply.github.com>
2022-04-09 19:29:57 -0300

    update

--- 2_volumes/index.php
+++ 2_volumes/index.php
@@ -7,7 +7,7 @@
   <title>Mensagens</title>
 </head>
 <body>
-  <h1>Escreva sua Mensagem! BIND MOUNT</h1>
+  <h1>Escreva sua Mensagem!</h1>
   <form action="process.php" method="POST">
     <input type="text" name="message" id="message">
     <input type="submit" value="Enviar Mensagem">
